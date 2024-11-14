3ac34076cfcc8aea5fedf35915a0d095062cbafc
Tomasz Kawalec <tkawalec369@gmail.com>
2024-11-14 18:57:40 +0100

    More WebSocket stuff - communication with ground station/simulator

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use futures_util::SinkExt;
+use futures_util::{SinkExt, StreamExt};
 use sdl2;
 use tokio;
 use tokio_tungstenite;
@@ -105,38 +105,102 @@ async fn main() -> tokio::io::Result<()> {
     .expect("Couldn't open WebSocket");
     println!("Opened WebSocket connection");
 
+    //let ws_stream = std::sync::Arc::new(tokio::sync::Mutex::new(ws_stream));
+
+    let (mut ws_stream_tx, mut ws_stream_rx) = ws_stream.split();
+
+    ws_stream_tx
+        .send(tokio_tungstenite::tungstenite::Message::Binary(
+            postcard::to_stdvec::<blimp_ground_ws_interface::MessageV2G>(
+                &blimp_ground_ws_interface::MessageV2G::DeclareInterest(
+                    blimp_ground_ws_interface::VisInterest {
+                        motors: true,
+                        servos: false,
+                    },
+                ),
+            )
+            .unwrap(),
+        ))
+        .await
+        .unwrap();
+
+    {
+        let mut shutdown_rx = shutdown_tx.subscribe();
+        //let ws_stream = ws_stream.clone();
+        tokio::spawn(async move {
+            loop {
+                tokio::select! {
+                    yoke_ev = yoke_rx.recv() => {
+                        println!("{:?}", yoke_ev);
+                        match yoke_ev {
+                            Some(YokeEvent::AxisMotion { axis, value }) => {},
+                            Some(YokeEvent::ButtonState { button, state }) => {},
+                            None => {
+                                break;
+                            }
+                        }
+                        let msg_ser = postcard::to_stdvec::<blimp_ground_ws_interface::MessageV2G>(
+                                    &blimp_ground_ws_interface::MessageV2G::Controls(
+                                        blimp_ground_ws_interface::Controls {
+                                            throttle: 0,
+                                            pitch: 0,
+                                            roll: 0,
+                                        },
+                                    ),
+                                ).unwrap();
+                        //println!("Serialized message");
+                        //let mut ws_stream_locked = ws_stream.lock().await;
+                        //println!("Locked WS stream");
+                        ws_stream_tx
+                            .send(tokio_tungstenite::tungstenite::Message::Binary(
+                               msg_ser,
+                            ))
+                            .await
+                            .unwrap();
+                        //println!("Send message");
+                    }
+                    _ = shutdown_rx.recv() => {
+                        break;
+                    }
+                }
+                //tokio::task::yield_now().await;
+            }
+        });
+    }
+
     tokio::spawn(async move {
         loop {
+            //let mut ws_stream_locked = ws_stream.lock().await;
             tokio::select! {
-                yoke_ev = yoke_rx.recv() => {
-                    println!("{:?}", yoke_ev);
-                    match yoke_ev {
-                        Some(YokeEvent::AxisMotion { axis, value }) => {},
-                        Some(YokeEvent::ButtonState { button, state }) => {},
-                        None => {
-                            break;
+                ws_msg = ws_stream_rx.next() => {
+                    //println!("Received some WS message: {:#?}", ws_msg);
+                    if let Some(ws_msg) = ws_msg{
+                        if let Ok(ws_msg) = ws_msg{
+                            println!("Got WS G2V message: {:#?}", ws_msg);
+                            match ws_msg {
+                                tokio_tungstenite::tungstenite::Message::Binary(msg_bin) => {
+                                    let msg_des = postcard::from_bytes::<blimp_ground_ws_interface::MessageG2V>(&msg_bin).unwrap();
+                                    match msg_des {
+                                        ms @ blimp_ground_ws_interface::MessageG2V::MotorSpeed { id, speed } => {
+                                            println!("Updated speed: {:#?}", ms);
+                                        }
+                                        _ => {}
+                                    }
+                                }
+                                _ => {}
+                            }
                         }
                     }
-                    ws_stream
-                        .send(tokio_tungstenite::tungstenite::Message::Binary(
-                            postcard::to_stdvec::<blimp_ground_ws_interface::MessageV2G>(
-                                &blimp_ground_ws_interface::MessageV2G::Controls(
-                                    blimp_ground_ws_interface::Controls {
-                                        throttle: 0,
-                                        pitch: 0,
-                                        roll: 0,
-                                    },
-                                ),
-                            )
-                            .unwrap(),
-                        ))
-                        .await
-                        .unwrap();
+                    else {
+                        println!("WebSocket connection closed!");
+                        break;
+                    }
                 }
                 _ = shutdown_rx.recv() => {
                     break;
                 }
-            }
+            };
+            //tokio::task::yield_now().await;
         }
     });
 
