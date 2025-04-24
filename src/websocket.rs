c4c85561216ac4087e76ee00b98cd0b94ea6961c
Tomasz Kawalec <tkawalec369@gmail.com>
2025-04-24 18:16:47 +0200

    Start changing code to use blimp_ground_ws_interface's client implementation - WIP, doesn't compile yet

--- src/websocket.rs
+++ src/websocket.rs
@@ -12,30 +12,20 @@ pub async fn ws_client_start(
     //TODO: Allow configuring WS address
     let ws_addr = "ws://127.0.0.1:8765";
 
-    let (mut ws_stream, _) = tokio_tungstenite::connect_async(
-        tokio_tungstenite::tungstenite::client::IntoClientRequest::into_client_request(ws_addr)
-            .expect(format!("Malformed WebSocket address: {}", ws_addr).as_str()),
-    )
-    .await
-    .expect(format!("Couldn't open WebSocket connection with {}", ws_addr).as_str());
+    let mut ws_client = blimp_ground_ws_interface::BlimpGroundWebsocketClient::new(ws_addr);
+    ws_client
+        .connect()
+        .await
+        .expect("Failed to connect to the WS server");
     println!("Opened WebSocket connection");
 
-    //let ws_stream = std::sync::Arc::new(tokio::sync::Mutex::new(ws_stream));
-
-    let (mut ws_stream_tx, mut ws_stream_rx) = ws_stream.split();
-
-    ws_stream_tx
-        .send(tokio_tungstenite::tungstenite::Message::Binary(
-            postcard::to_stdvec::<blimp_ground_ws_interface::MessageV2G>(
-                &blimp_ground_ws_interface::MessageV2G::DeclareInterest(
-                    blimp_ground_ws_interface::VizInterest {
-                        motors: true,
-                        servos: false,
-                        sensors: false,
-                    },
-                ),
-            )
-            .unwrap(),
+    ws_client
+        .send(blimp_ground_ws_interface::MessageV2G::DeclareInterest(
+            blimp_ground_ws_interface::VizInterest {
+                motors: true,
+                servos: false,
+                sensors: false,
+            },
         ))
         .await
         .unwrap();
@@ -66,25 +56,14 @@ pub async fn ws_client_start(
                                 break;
                             }
                         }
-                        let msg_ser = postcard::to_stdvec::<blimp_ground_ws_interface::MessageV2G>(
-                                    &blimp_ground_ws_interface::MessageV2G::Controls(
-                                        blimp_ground_ws_interface::Controls {
-                                            throttle: *axes_values.get(&crate::BlimpSteeringAxis::Throttle).unwrap_or(&0),
-                                            elevation: *axes_values.get(&crate::BlimpSteeringAxis::Elevation).unwrap_or(&0),
-                                            yaw: *axes_values.get(&crate::BlimpSteeringAxis::Yaw).unwrap_or(&0),
-                                        },
-                                    ),
-                                ).unwrap();
-                        //println!("Serialized message");
-                        //let mut ws_stream_locked = ws_stream.lock().await;
-                        //println!("Locked WS stream");
-                        ws_stream_tx
-                            .send(tokio_tungstenite::tungstenite::Message::Binary(
-                               msg_ser,
-                            ))
-                            .await
-                            .unwrap();
-                        //println!("Send message");
+
+                        ws_client.send(blimp_ground_ws_interface::MessageV2G::Controls(
+                            blimp_ground_ws_interface::Controls {
+                                throttle: *axes_values.get(&crate::BlimpSteeringAxis::Throttle).unwrap_or(&0),
+                                elevation: *axes_values.get(&crate::BlimpSteeringAxis::Elevation).unwrap_or(&0),
+                                yaw: *axes_values.get(&crate::BlimpSteeringAxis::Yaw).unwrap_or(&0),
+                            },
+                        )).await.unwrap();
                     }
                     _ = shutdown_rx.recv() => {
                         break;
@@ -102,23 +81,15 @@ pub async fn ws_client_start(
             loop {
                 //let mut ws_stream_locked = ws_stream.lock().await;
                 tokio::select! {
-                    ws_msg = ws_stream_rx.next() => {
+                    ws_msg = ws_client.recv() => {
                         //println!("Received some WS message: {:#?}", ws_msg);
-                        if let Some(ws_msg) = ws_msg{
-                            if let Ok(ws_msg) = ws_msg{
-                                //println!("Got WS G2V message: {:#?}", ws_msg);
-                                match ws_msg {
-                                    tokio_tungstenite::tungstenite::Message::Binary(msg_bin) => {
-                                        let msg_des = postcard::from_bytes::<blimp_ground_ws_interface::MessageG2V>(&msg_bin).unwrap();
-                                        match msg_des {
-                                            ms @ blimp_ground_ws_interface::MessageG2V::MotorSpeed { id, speed } => {
-                                                // println!("Updated speed: {:#?}", ms);
-                                            }
-                                            _ => {}
-                                        }
-                                    }
-                                    _ => {}
+                        if let Ok(ws_msg) = ws_msg{
+                            //println!("Got WS G2V message: {:#?}", ws_msg);
+                            match ws_msg {
+                                ms @ blimp_ground_ws_interface::MessageG2V::MotorSpeed { id, speed } => {
+                                    // println!("Updated speed: {:#?}", ms);
                                 }
+                                _ => {}
                             }
                         }
                         else {
